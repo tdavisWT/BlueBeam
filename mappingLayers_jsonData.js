45d7f8084c024e4e29a40aad4c6dc810c030088a
whitingTurner <jeevjyot.chhabda@whiting-turner.com>
2016-07-25 08:37:12 -0700

    Added later, mapping

---
+++ mappingLayers_jsonData.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jeevjyot on 7/22/16.
+ */
+//@Author: Jeevjyot Singh Chhabda
+
+////////////////////////////////////////////////////////////////////////////////
+//This script the read json data from the excel file
+//maps ths JSON data from OCG present in the bluebeam PDF
+//one the layers are matched, each layer is assigned the data present in the json
+//such as state (ON or OFF)
+//and display the results on the PDF sheet
+///////////////////////////////////////////////////////////////////////////////
